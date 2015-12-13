5af6c98e07b122133c0c71c670fb6059274ca0ad
Zach Bergson <zach.bergson@gmail.com>
2015-12-13 15:43:17 -0500

    second visualizations complete

---
+++ charts/app.js
@@ -0,0 +1,156 @@
+$(document).ready(function(){
+
+
+  var getDrones = function() {
+
+    var urlDrone = 'http://api.dronestre.am/data';
+      
+
+    $.ajax({
+      url: urlDrone,
+      crossOrigin: true,
+      type: "GET",
+      dataType: 'jsonp'
+    }).done(function(data){
+      makeChart(data);
+        $('#submit').click(function(){
+          event.preventDefault();
+          makeChart(data);
+        });
+    });
+
+  }
+
+  getDrones();
+
+
+
+
+  var makeChart = function(data) {
+    var time = moment(data.strike[0]['date']).format('YYYY');
+    console.log(parseInt(time));
+
+    data = data.strike
+
+    var data_array = [];
+
+    data.forEach(function(data){
+      var country = {"country" : data.country, "deaths" : parseInt(data.deaths_max), "year" : 
+      parseInt(moment(data.date).format('YYYY')), "region" : data.location, "drone-strikes" : 1};
+      data_array.push(country);
+    });
+
+    console.log(data_array);
+
+    var country = $('#country').prop('checked');
+
+    if (($('#country').prop('checked') === false && $('#year').prop('checked') === false && $('#region').prop('checked') === false && $('#number').prop('checked') === false)  || $('#year').prop('checked') === true) {
+
+      var visualization = d3plus.viz()
+      .container('#viz')
+      .data(data_array)
+      .type("stacked")
+      .messages( "Loading..." )
+      .id("country")
+      .title('Drone Deaths Between 2002-present')
+      .text("country")
+      .y("deaths")
+      .x("year")
+      .ui([{ 
+        "label": "Visualization Type",
+        "method": "type", 
+        "value": ["stacked", "scatter"]
+      }])
+      .time({"value": "year"})
+      .timeline({"hover": "grab"})
+      .timing({"transitions" : "1500"})
+      .draw()
+
+      $('#checkboxes').show();
+
+    }
+
+    else if (($('#country').prop('checked') === true) && ($('#region').prop('checked') === false && $('#year').prop('checked') === false && $('#number').prop('checked') === false)) {
+
+      var visualization = d3plus.viz()
+      .container('#viz')
+      .data(data_array)
+      .type("bar")
+      .messages( "Loading..." )
+      .id("country")
+      .title('Drone Deaths Between 2002-present')
+      .text("deaths")
+      .y("deaths")
+      .x("country")
+      .timing({"transitions" : "1500"})
+      .ui([{ 
+        "label": "Visualization Type",
+        "method": "type", 
+        "value": ["bar", "box"]
+      }])
+      .draw()
+
+      $('#checkboxes').show();
+
+    }
+
+    else if (($('#region').prop('checked') === true) && ($('#country').prop('checked') === false && $('#year').prop('checked') === false && $('#number').prop('checked') === false) ) {
+
+      var visualization = d3plus.viz()
+      .container('#viz')
+      .data(data_array)
+      .type("bubbles")
+      .messages( "Loading..." )
+      .id(["region", "country"])
+      .title('Drone Deaths Between 2002-present')
+      // .text("region")
+      .size("deaths")
+      .color("country")
+      .legend({"size" : 50})
+      .timing({"transitions" : "1500"})
+      .draw()
+
+      $('#checkboxes').show();
+
+    }
+
+    else if (($('#number').prop('checked') === true) && ($('#country').prop('checked') === false && $('#year').prop('checked') === false && $('#region').prop('checked') === false) ) {
+
+      var visualization = d3plus.viz()
+      .container('#viz')
+      .data(data_array)
+      .type("line")
+      .messages( "Loading..." )
+      .id("country")
+      .title('Number of Drone Strikes in Pakistan, Yemen and Somalia Between 2002-Present')
+      .text("country")
+      .y("drone-strikes")
+      .x("year")
+      .time({"value": "year"})
+      .ui([{ 
+        "label": "Visualization Type",
+        "method": "type", 
+        "value": ["line", "scatter", "stacked"]
+      }])
+      .timeline({"hover": "grab"})
+      .timing({"transitions" : "1500"})
+      .draw()
+
+      $('#checkboxes').show();
+
+    }
+
+    else {
+      alert('please check one box!');
+    }
+
+
+  }
+
+
+
+
+
+
+});
+
